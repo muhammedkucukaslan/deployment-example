0a3ff29a511e72b898d4cb85ab0b028b3b5e3447
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-07-10 23:50:20 +0300

    feat: add handler

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Math package is ready to use.")
+	http.HandleFunc("/add", AdditionHandler)
+	http.ListenAndServe(":3000", nil)
+
 }
