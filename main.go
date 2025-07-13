3859b9c8133885d817b572ac43be2a0512debcf3
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-07-13 08:06:18 +0300

    fix: add routes

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	fmt.Println("Math package is ready to use.")
 	http.HandleFunc("/add", AdditionHandler)
+	http.HandleFunc("/hello", HelloWorldHandler)
+	http.HandleFunc("/square", SquareHandler)
+	fmt.Println("Server is running on http://localhost:3000")
 	http.ListenAndServe("0.0.0.0:3000", nil)
 
 }
